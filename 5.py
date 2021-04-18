da070feef66fee3647d47969c35090bf69e790f2
memanesavita <82706660+memanesavita@users.noreply.github.com>
2021-04-18 19:49:57 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,51 @@
+# varx=int(input("enter the nuber"))
+# if(varx==77):
+#     print("barabar hai")
+# else:
+#     print("barabar nahi hai")
+
+
+# calculater
+ 
+# num1=int(input("enter the num1"))
+# num2=int(input("enter the num2"))
+# symbole=(input("enter the symbole"))
+# if symbole=="+":
+#     print(num1+num2)
+# elif symbole=="-":
+#     print(num1-num2)
+# elif symbole=="*":
+#     print(num1*num2)
+# elif symbole=="/":
+#     print(num1/num2)
+# elif symbole=="%":
+#     print(num1%num2)
+# elif symbole=="a**b":
+#     print(num1**num2)
+# elif symbole=="a//b":
+#     print(num1//num2)
+# else :
+#     print(envaet)
+
+
+
+# user=input("enter the season:")
+# if user=="november" or user=="december" or user=="january" or user=="febraury":
+#     print("it is winner season")
+# elif user=="march" or user=="april" or user=="may":
+#     print("summer season")
+# elif user=="june" or user=="july" or user=="august":
+#     print("it is rainy season")
+# else:
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ if-elase.divigible.py
@@ -0,0 +1,13 @@
+
+
+
+
+#  DIVISIBLE AND NOT DIVISIBLE
+#
+#  # varx=int(input("enter thae number"))
+# if(varx%2==0):
+#     print("divisible hai")
+# else:
+#     print("divisible nahi hai")
+
+
